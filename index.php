8ba0924ef6f1e7e6184e39327d0ee15953264bd1
Your Name <you@example.com>
2022-06-01 15:02:14 +0200

    added files

---
+++ index.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="./css/index.css">
+
+<?php include 'header.php' ?>
+
+<form action="laptops.php">
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+
+   
+      
+</div>
+</form>
+
+
+
+
+<?php include 'footer.php' ?>
+
+  </body>
